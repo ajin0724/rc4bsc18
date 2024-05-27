df7751ea5fb60d1c17412b32b4cfecfe5a825d44
ajin0724 <121547943+ajin0724@users.noreply.github.com>
2024-05-28 00:04:55 +0900

    Add files via upload

---
+++ pretraining_dino.py
@@ -0,0 +1,142 @@
+# Note: The model and training settings do not follow the reference settings
+# from the paper. The settings are chosen such that the example can easily be
+# run on a small dataset with a single GPU.
+
+import copy
+import torch
+import torchvision
+from torch import nn
+
+from lightly.loss import DINOLoss
+from lightly.models.modules import DINOProjectionHead
+from lightly.models.utils import deactivate_requires_grad, update_momentum
+from lightly.transforms.dino_transform import DINOTransform
+from lightly.utils.scheduler import cosine_schedule
+
+import os
+
+
+class DINO(torch.nn.Module):
+    def __init__(self, backbone, input_dim):
+        super().__init__()
+        self.student_backbone = backbone
+        self.student_head = DINOProjectionHead(
+            input_dim, 512, 64, 2048, freeze_last_layer=1
+        )
+        self.teacher_backbone = copy.deepcopy(backbone)
+        self.teacher_head = DINOProjectionHead(input_dim, 512, 64, 2048)
+        deactivate_requires_grad(self.teacher_backbone)
+        deactivate_requires_grad(self.teacher_head)
+
+    def forward(self, x):
+        y = self.student_backbone(x).flatten(start_dim=1)
+        z = self.student_head(y)
+        return z
+
+    def forward_teacher(self, x):
+        y = self.teacher_backbone(x).flatten(start_dim=1)
+        z = self.teacher_head(y)
+        return z
+
+from collections import OrderedDict
+resnet = torchvision.models.resnet18()
+backbone = nn.Sequential(OrderedDict(list(resnet.named_children())[:-1]))
+backbone = nn.DataParallel(backbone)
+input_dim = 512
+# instead of a resnet you can also use a vision transformer backbone as in the
+# original paper (you might have to reduce the batch size in this case):
+# backbone = torch.hub.load('facebookresearch/dino:main', 'dino_vits16', pretrained=False)
+# input_dim = backbone.embed_dim
+
+model = DINO(backbone, input_dim)
+
+device = torch.device('cuda:{}'.format(0))
+model.to(device)
+
+# 전처리 과정
+transform = DINOTransform()
+dataset = torchvision.datasets.ImageFolder(root='dataset', transform=transform)
+print(dataset)
+
+# we ignore object detection annotations by setting target_transform to return 0
+# dataset = torchvision.datasets.VOCDetection(
+#     "datasets/pascal_voc",
+#     download=True,
+#     transform=transform,
+#     target_transform=lambda t: 0,
+# )
+# or create a dataset from a folder containing images or videos:
+# dataset = LightlyDataset("path/to/folder")
+
+from torch.utils.data import Dataset
+import natsort
+import os
+import zipfile
+from PIL import Image
+
+# class CustomDataSet(Dataset):
+#     def __init__(self, main_dir, transform):
+#         self.main_dir = main_dir
+#         self.transform = transform
+#         all_imgs = os.listdir(main_dir)
+#         self.total_imgs = natsort.natsorted(all_imgs)
+
+#     def __len__(self):
+#         return len(self.total_imgs)
+
+#     def __getitem__(self, idx):
+#         img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
+#         image = Image.open(img_loc).convert("RGB")
+#         tensor_image = self.transform(image)
+#         return tensor_image
+
+# dataset = CustomDataSet('/home/bsc18/workspace/Hyunjin/dataset', transform=transform)
+
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=64,
+    shuffle=True,
+    drop_last=True,
+    num_workers=8,
+)
+
+criterion = DINOLoss(
+    output_dim=2048,
+    warmup_teacher_temp_epochs=5,
+)
+# move loss to correct device because it also contains parameters
+criterion = criterion.to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 50
+
+print("Starting Training")
+for epoch in range(epochs):
+    total_loss = 0
+    momentum_val = cosine_schedule(epoch, epochs, 0.996, 1)
+    for batch in dataloader:
+        views = batch[0]
+        update_momentum(model.student_backbone, model.teacher_backbone, m=momentum_val)
+        update_momentum(model.student_head, model.teacher_head, m=momentum_val)
+        views = [view.to(device) for view in views]
+        global_views = views[:2]
+        teacher_out = [model.forward_teacher(view) for view in global_views]
+        student_out = [model.forward(view) for view in views]
+        loss = criterion(teacher_out, student_out, epoch=epoch)
+        total_loss += loss.detach()
+        loss.backward()
+        # We only cancel gradients of student head.
+        model.student_head.cancel_last_layer_gradients(current_epoch=epoch)
+        optimizer.step()
+        optimizer.zero_grad()
+
+    avg_loss = total_loss / len(dataloader)
+    print(f"epoch: {epoch:>02}, loss: {avg_loss:.5f}")
+
+save_dir = "saved_models"
+dino_checkpoint_path = os.path.join(save_dir, "dino_model_50.pth")
+student_backbone_checkpoint_path = os.path.join(save_dir, "student_backbone_50.pth")
+# nn.Sequential(*list(model.children())[:-1])
+torch.save(model.student_backbone.state_dict(), student_backbone_checkpoint_path)
+torch.save(model.state_dict(), dino_checkpoint_path)
